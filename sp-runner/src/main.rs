6e872941e9cbba1eb26158da2fb0f3c8ecc3a622
Kristofer Bengtsson <kristofer@sekvensa.se>
2019-06-27 10:44:45 +0200

    A working buffer

--- sp-runner/src/main.rs
+++ sp-runner/src/main.rs
@@ -2,191 +2,70 @@ extern crate tokio;
 
 use tokio::*;
 use tokio::prelude::*;
-
 use std::collections::*;
-use std::sync::{Arc, Mutex};
-use sync::mpsc;
-use futures::try_ready;
 
 mod streamsupport;
-
-type State = HashMap<String, usize>;
-
-#[derive(Debug)]
-struct InputMessages {
-    input: mpsc::Receiver<State>, //Incoming messages from ROS
-    output: mpsc::Sender<State>, // Outgoing to runner
-    length: usize,                // the length of the buffer. Will pack-pressure incoming
-    q: VecDeque<State>,            // the buffer for states
-}
+use streamsupport::messagebuffer::*;
 
 
-
-impl InputMessages {
-    /// In this method the state should be merged if possible, else added into the queue
-    /// if queue becomes to big if the consumer is always slower than producer, 
-    /// handle that here by an aggressive merge
-    fn new_message(&mut self, m: State) {
-
-        // if you do not want to merge, uncomment and comment the merge
-         self.q.push_back(m);
-
-        // if you want to merge
-        // if self.q.is_empty() || (self.q.back().unwrap().get("counter").unwrap() % 10) == 0 {
-        //     self.q.push_back(m);
-        // } else {
-        //     let new_count = m.get("counter").unwrap();
-        //     let b = self.q.back_mut().unwrap();
-        //     let prev = b.get("tjo").unwrap_or(&1) + new_count;
-        //     b.insert("tjo".to_string(), prev);
-        //     b.insert("counter".to_string(), *new_count);
-        // }
-
-        
-        println!("new message current que {:?}", self.q)
-    }
-    
-    fn has_message_to_send(& self) -> bool {
-        !self.q.is_empty()
-    }
-    fn message_to_send(&mut self) -> Option<State> {
-        self.q.pop_front()
-    }
-
-    /// This method tries to read all messages that are waiting
-    fn get_message(&mut self) -> Result<Async<()>, sync::mpsc::error::RecvError> {
-        loop {
-            if (self.q.len() >= self.length) {
-                //task::current().notify();
-                return Ok(Async::NotReady);
-            }
-            let x = try_ready!(self.input.poll());
-            x.map(|mess| self.new_message(mess));
-        }
-    }
-
-
-}
-
-impl Future for InputMessages {
-    type Item = ();
-    type Error = ();
-
-    fn poll(&mut self) -> Poll<(), ()> {
-        
-        println!();
-        println!("*******************");
-
-
-        let x = self.get_message();
-        println!("fetch {:?}", x);
-
-        match self.output.poll_ready() {
-            Ok(Async::NotReady) => {
-                println!("NotReady");
-                return Ok(Async::NotReady);
-            }
-            Ok(Async::Ready(_)) => {
-                println!("Ready");
-
-                if self.has_message_to_send() {
-                    let mess = self.message_to_send().unwrap();
-                    println!();
-                    println!("I am SENDING: {:?}", mess);
-                    println!();
-                    self.output.try_send(mess).unwrap();
-                }
-
-                if self.has_message_to_send() {task::current().notify();}
-                return Ok(Async::NotReady);
-
-            }
-            Err(_) => panic!("outgoing message channel terminated")
-        }
-
-        println!();
-
-    }
-}
-
-#[macro_export]
-macro_rules! state {
-    ($( $key: expr => $val: expr ),*) => {{
-         let mut map = ::std::collections::HashMap::new();
-         $( map.insert($key, $val); )*
-         map
-    }}
-}
 
 
 fn main() {
-    let byte_stream = codec::FramedRead::new(tokio::io::stdin(), codec::LinesCodec::new());
-    let (toBuf, incomingBuf) = tokio::sync::mpsc::channel(2);
-    let (fromBuf, mut output) = tokio::sync::mpsc::channel(2);
-    let buf = InputMessages {
-        input: incomingBuf,
-        output: fromBuf,
-        length: 5,
-        q: VecDeque::new(),
-    };
 
-    use tokio::timer::Interval;
-    use std::time::{Duration, Instant};
+    // some example how to fetch from terminal
+    //let byte_stream = codec::FramedRead::new(tokio::io::stdin(), codec::LinesCodec::new());
 
-    let task = Interval::new(Instant::now(), Duration::from_millis(500))
-        .map(|x|{println!("hej"); x})
-        .map_err(|_| ())
-        .zip(output.map_err(|_| ()))
-        .for_each(|(_, res)| {
-            println!("fire; yes={:?}", res);
-            Ok(())
-        });
+    // use tokio::timer::Interval;
+    // use std::time::{Duration, Instant};
 
+    // let task = Interval::new(Instant::now(), Duration::from_millis(500))
+    //     .map(|x|{println!("hej"); x})
+    //     .map_err(|_| ())
+    //     .zip(from_buf.map_err(|_| ()))
+    //     .for_each(|(_, res)| {
+    //         println!("fire; yes={:?}", res);
+    //         Ok(())
+    //     });
 
+    //         let inputStream = byte_stream
+    //     .map(move  |line| {
+    //         match line.as_ref() {
+    //             "a" => {
+    //                 for x in 0..20 {
+    //                     let s = state!("counter".to_string() => x);
+    //                     let send = to_buf
+    //                         .clone()
+    //                         .send(s)
+    //                         .map(move |_| ())
+    //                         .map_err(|e| eprintln!("error = {:?}", e));
+    //                     tokio::spawn(send);
+    //                 };
+    //                 println!{"Yes A"};
+    //             }
+    //             "b" => {
+    //                 println!{"Yes B"};
+    //             }
+    //             x => {println!{"You wrote: {:?}", x}}
+    //         }
+    //         line
+    //     });
 
-    let mut aState: State = state!("kalle".to_string() => 1, "pelle".to_string() => 2);
-    let mut counter = 0;
+    //     tokio::run( future::lazy(|| {
+    //         tokio::spawn(buf);
 
-    let inputStream = byte_stream
-        .map(move  |line| {
-            match line.as_ref() {
-                "a" => {
-                    for x in 0..100 {
-                        let s = state!("counter".to_string() => x);
-                        let send = toBuf
-                            .clone()
-                            .send(s)
-                            .map(move |_| ())
-                            .map_err(|e| eprintln!("error = {:?}", e));
-                        tokio::spawn(send);
-                    };
-                    println!{"Yes A"};
-                }
-                "b" => {
-                    println!{"Yes B"};
-                }
-                x => {println!{"You wrote: {:?}", x}}
-            }
-            line
-        });
+    //         tokio::spawn(
+    //             task
+    //         );
 
-    println!("Starting.....");
-
-    tokio::run( future::lazy(|| {
-        tokio::spawn(buf);
-
-        tokio::spawn(
-            task
-        );
-
-        tokio::spawn(
-            inputStream
-            .take_while(|x| Ok(x != "exit"))
-            .for_each(|x| {
-                Ok(())
-            })
-            .map_err(|e| println!("Error {:?}", e))
-        )
+    //         tokio::spawn(
+    //             inputStream
+    //             .take_while(|x| Ok(x != "exit"))
+    //             .for_each(|x| {
+    //                 Ok(())
+    //             })
+    //             .map_err(|e| println!("Error {:?}", e))
+    //         )
         
-    }));
+    //     }));
+
 }
--- sp-runner/src/streamsupport/messagebuffer.rs
+++ sp-runner/src/streamsupport/messagebuffer.rs
@@ -1,10 +1,12 @@
+#![allow(dead_code)]
+
 use tokio::*;
 use tokio::prelude::*;
 
 use std::collections::*;
-use std::sync::{Arc, Mutex};
 use sync::mpsc;
 use futures::try_ready;
+
 
 /// Buffers and merges messages to handle slower consumers
 /// 
@@ -12,29 +14,49 @@ use futures::try_ready;
 /// (Maybe we instead should always poll and merge aggressive if buffer is full)
 
 
-pub struct MessageBuffer<T> {
-    in_R: mpsc::Receiver<T>, //Incoming messages from ROS
-    out_S: mpsc::Sender<T>, // Outgoing to runner
+pub struct MessageBuffer<T> where T: std::fmt::Debug {
+    in_r: mpsc::Receiver<T>, //Incoming messages from ROS
+    out_s: mpsc::Sender<T>, // Outgoing to runner
     buffer_size: usize,                // the length of the buffer. Will pack-pressure incoming
     q: VecDeque<T>,            // the buffer for states
     merge: fn(&mut T, &T) -> bool  // Mutate last T if new T should be merged. Return true if merged
 }
 
-impl<T> MessageBuffer<T> {
-
+impl<T> MessageBuffer<T> where T: std::fmt::Debug {
     pub fn new (
-        in_R: mpsc::Receiver<T>, 
-        out_S: mpsc::Sender<T>,
+        buffer_size: usize,
+        merge: fn(&mut T, &T) -> bool
+    ) -> (MessageBuffer<T>, mpsc::Sender<T>, mpsc::Receiver<T>) {
+        let (to_buf, into_buf) = tokio::sync::mpsc::channel::<T>(1);
+        let (outof_buf, from_buf) = tokio::sync::mpsc::channel::<T>(1);
+
+        (
+            MessageBuffer{
+                in_r: into_buf, 
+                out_s: outof_buf, 
+                buffer_size, 
+                q: (VecDeque::new()), 
+                merge},
+            to_buf,
+            from_buf
+        )
+    }
+
+    pub fn new_with_channel (
+        in_r: mpsc::Receiver<T>, 
+        out_s: mpsc::Sender<T>,
         buffer_size: usize,
         merge: fn(&mut T, &T) -> bool
     ) -> MessageBuffer<T> {
-        MessageBuffer{in_R, out_S, buffer_size, q: (VecDeque::new()), merge}
+        MessageBuffer{in_r, out_s, buffer_size, q: (VecDeque::new()), merge}
     }
+
 
     /// In this method the message should be merged if possible, else added into the queue
     /// if queue becomes to big if the consumer is always slower than producer, maybe
     /// handle that here by an aggressive merge
     fn new_message(&mut self, m: T) {
+        //println!("message to que: {:?}", m);
         if self.q.is_empty()  {
             self.q.push_back(m);
         } else {
@@ -54,13 +76,20 @@ impl<T> MessageBuffer<T> {
     }
 
     /// This method tries to read all messages that are waiting
-    fn get_message(&mut self) -> Result<Async<()>, sync::mpsc::error::RecvError> {
+    fn get_message(&mut self) -> Result<Async<Option<()>>, sync::mpsc::error::RecvError> {
         loop {
             if self.q.len() >= self.buffer_size {
                 return Ok(Async::NotReady);
             }
-            let x = try_ready!(self.in_R.poll());
-            x.map(|mess| self.new_message(mess));
+            let res = try_ready!(self.in_r.poll());
+            //println!("We got: {:?}", res);
+            match res {
+                Some(mess) => {
+                    self.new_message(mess)
+                }
+                None => {return Ok(Async::Ready(None))}
+            }
+
         }
     }
 
@@ -68,52 +97,207 @@ impl<T> MessageBuffer<T> {
 }
 
 /// A long living future that never returns
-impl<T> Future for MessageBuffer<T> {
+impl<T> Future for MessageBuffer<T> where T: std::fmt::Debug  {
     type Item = (); 
     type Error = ();
 
     fn poll(&mut self) -> Poll<(), ()> {
-        let x = self.get_message();
+        let getting = self.get_message();
 
-        match self.out_S.poll_ready() {
-            Ok(Async::NotReady) => {
-                Ok(Async::NotReady)
+        let is_out_ready = self.out_s.poll_ready();
+        if let Ok(Async::Ready(_)) =  is_out_ready {
+            //println!("kö Ready: {:?}", self.has_message_to_send());
+            if self.has_message_to_send() {
+                let mess = self.message_to_send().unwrap();
+                self.out_s.try_send(mess); // should always work
             }
-            Ok(Async::Ready(_)) => {
-                if self.has_message_to_send() {
-                    let mess = self.message_to_send().unwrap();
-                    self.out_S.try_send(mess);
+            if self.has_message_to_send() {task::current().notify();}
+        };
+
+        if let Err(e) = is_out_ready {
+            eprintln!("MessageBuffer: We have an error in outgoing {:?}", e)
+        };
+
+        match getting {
+            Ok(Async::Ready(None)) if self.q.is_empty() => {
+                Ok(Async::Ready(()))
+            }
+            Err(e) => {
+                eprintln!("MessageBuffer: We have an error in incoming {:?}", e);
+                panic!("o no")
                 }
-
-                if self.has_message_to_send() {task::current().notify();}
+            _ => {
                 Ok(Async::NotReady)
             }
-            Err(_) => panic!("outgoing message channel terminated")
         }
     }
 }
+
+
+
+/// ********** TESTS ***************
 
 #[cfg(test)]
 mod messagebuffer_test {
     use super::*;
     #[test]
     fn create() {
-        let (toBuf, incomingBuf) = tokio::sync::mpsc::channel::<String>(2);
-        let (fromBuf, mut output) = tokio::sync::mpsc::channel::<String>(2);
+        tokio::run( future::lazy(move || {
 
-        let newBuf = MessageBuffer::new(incomingBuf, fromBuf, 5, |last, next| {
-            false // new merge
+            let (buf, to_buf, from_buf) = MessageBuffer::new(2, |last: &mut String, next: &String| {
+                false // no merge
+            });
+
+            let range = vec!("one", "one", "two", "three", "four");
+            let sending = stream::iter_ok(range).for_each(move |s|{
+                let send = to_buf
+                        .clone()
+                        .send(s.to_string())
+                        .map(move |_| ())
+                        .map_err(|e| eprintln!("error = {:?}", e));
+                tokio::spawn(send)
+            });
+            tokio::spawn(sending);
+
+            let getting = from_buf.collect().map(|result| {
+                    println!("Yes: {:?}", result);
+                    assert_eq!(result, vec!("one", "one", "two", "three", "four"));
+                }).map(move |_| ()).map_err(|e| eprintln!("error = {:?}", e));
+            tokio::spawn(getting);
+
+            tokio::spawn(buf)
+            
+        }));
+        
+    }
+
+    #[test]
+    fn new_with_channel() {
+        tokio::run( future::lazy(move || {
+            let (to_buf, into_buf) = tokio::sync::mpsc::channel::<String>(1);
+            let (outof_buf, from_buf) = tokio::sync::mpsc::channel::<String>(1);
+
+            let buf = MessageBuffer::new_with_channel(into_buf, outof_buf, 2, |_, _| {
+                false // no merge
+            });
+
+            let range = vec!("one", "one", "two", "three", "four");
+            let sending = stream::iter_ok(range).for_each(move |s|{
+                let send = to_buf
+                        .clone()
+                        .send(s.to_string())
+                        .map(move |_| ())
+                        .map_err(|e| eprintln!("error = {:?}", e));
+                tokio::spawn(send)
+            });
+            tokio::spawn(sending);
+
+            let getting = from_buf.collect().map(|result| {
+                    println!("Yes: {:?}", result);
+                    assert_eq!(result, vec!("one", "one", "two", "three", "four"));
+                }).map(move |_| ()).map_err(|e| eprintln!("error = {:?}", e));
+            tokio::spawn(getting);
+
+            tokio::spawn(buf)
+            
+        }));
+        
+    }
+
+    type State = HashMap<String, usize>;
+    #[macro_export]
+    macro_rules! state {
+        ($( $key: expr => $val: expr ),*) => {{
+            let mut map = ::std::collections::HashMap::new();
+            $( map.insert($key, $val); )*
+            map
+        }}
+    }
+
+    #[test]
+    fn slow_consumer_test_with_merge() {
+    
+    let (buf, to_buf, from_buf) = MessageBuffer::new(5, |last: &mut State, next: &State| {
+            let new_count = next.get("counter").unwrap();
+            let prev = last.get("tjo").unwrap_or(&1) + new_count;
+            last.insert("tjo".to_string(), prev);
+            last.insert("counter".to_string(), *new_count);
+            true
         });
 
+    use tokio::timer::Interval;
+    use std::time::{Duration, Instant};
+
+    let task = Interval::new(Instant::now(), Duration::from_millis(100))
+        .map_err(|_| ())
+        .zip(from_buf.map_err(|_| ()))
+        .collect()
+        .map(|x|{
+            println!("We got: {:?}", x);
+            assert!(x.len() < 5); // should merge
+        });
         
 
-        println!("test: {:?}", newBuf.in_R)
+
+    println!("Starting.....");
+
+    tokio::run( future::lazy(move || {
+        tokio::spawn(buf);
+
+        (0..20).for_each(|x| {
+            let s = state!("counter".to_string() => x);
+            let send = to_buf
+                .clone()
+                .send(s)
+                .map(move |_| ())
+                .map_err(|e| eprintln!("error = {:?}", e));
+            tokio::spawn(send);
+        });
+
+        tokio::spawn(task)
+    }));
+
     }
+
+    #[test]
+    fn slow_consumer_test_no_merge() {
+    
+    let (buf, to_buf, from_buf) = MessageBuffer::new(10, |_: &mut State, _: &State| {
+            false
+        });
+
+    use tokio::timer::Interval;
+    use std::time::{Duration, Instant};
+
+    let task = Interval::new(Instant::now(), Duration::from_millis(100))
+        .map_err(|_| ())
+        .zip(from_buf.map_err(|_| ()))
+        .collect()
+        .map(|x|{
+            println!("We got: {:?}", x.len());
+            assert!(x.len() == 20); // should merge
+        });
+        
+
+
+    println!("Starting.....");
+
+    tokio::run( future::lazy(move || {
+        tokio::spawn(buf);
+
+        (0..20).for_each(|x| {
+            let s = state!("counter".to_string() => x);
+            let send = to_buf
+                .clone()
+                .send(s)
+                .map(move |_| ())
+                .map_err(|e| eprintln!("error = {:?}", e));
+            tokio::spawn(send);
+        });
+
+        tokio::spawn(task)
+    }));
+
+    }
+
 }
-
-
-            // let new_count = m.get("counter").unwrap();
-            // let b = self.q.back_mut().unwrap();
-            // let prev = b.get("tjo").unwrap_or(&1) + new_count;
-            // b.insert("tjo".to_string(), prev);
-            // b.insert("counter".to_string(), *new_count);
