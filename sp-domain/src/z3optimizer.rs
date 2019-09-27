4456bbbc6de2451d24d338547700c7ed595bbb20
endre90 <endre90@gmail.com>
2019-09-27 14:16:31 +0200

    max, min and working example

--- sp-domain/src/z3optimizer.rs
+++ sp-domain/src/z3optimizer.rs
@@ -16,6 +16,20 @@ pub struct OptAssertZ3<'ctx, 'opt> {
     pub r: ()
 }
 
+pub struct OptMaximizeZ3<'ctx, 'opt> {
+    pub ctx: &'ctx ContextZ3,
+    pub opt: &'opt OptimizerZ3<'ctx>,
+    pub cst: Z3_ast,
+    pub r: ()
+}
+
+pub struct OptMinimizeZ3<'ctx, 'opt> {
+    pub ctx: &'ctx ContextZ3,
+    pub opt: &'opt OptimizerZ3<'ctx>,
+    pub cst: Z3_ast,
+    pub r: ()
+}
+
 pub struct OptCheckZ3<'ctx, 'opt> {
     pub ctx: &'ctx ContextZ3,
     pub opt: &'opt OptimizerZ3<'ctx>,
@@ -61,6 +75,48 @@ impl <'ctx, 'opt> OptAssertZ3<'ctx, 'opt> {
             cst,
             r: unsafe {
                 Z3_optimize_assert(ctx.r, opt.r, cst);
+            }
+        }
+    }
+}
+
+impl <'ctx, 'opt> OptMaximizeZ3<'ctx, 'opt> {
+    /// Add a maximization constraint.
+    /// - `ctx`: - context
+    /// - `opt`: - optimization context
+    /// - `cst`: - arithmetical term
+    ///
+    /// # See also:
+    ///
+    /// - [`Z3_optimize_minimize`](fn.Z3_optimize_minimize.html)
+    pub fn new(ctx: &'ctx ContextZ3, opt: &'opt OptimizerZ3<'ctx>, cst: Z3_ast) -> OptMaximizeZ3<'ctx, 'opt> {
+        OptMaximizeZ3 {
+            ctx,
+            opt,
+            cst,
+            r: unsafe {
+                Z3_optimize_maximize(ctx.r, opt.r, cst);
+            }
+        }
+    }
+}
+
+impl <'ctx, 'opt> OptMinimizeZ3<'ctx, 'opt> {
+    /// Add a minimization constraint.
+    /// - `ctx`: - context
+    /// - `opt`: - optimization context
+    /// - `cst`: - arithmetical term
+    ///
+    /// # See also:
+    ///
+    /// - [`Z3_optimize_minimize`](fn.Z3_optimize_minimize.html)
+    pub fn new(ctx: &'ctx ContextZ3, opt: &'opt OptimizerZ3<'ctx>, cst: Z3_ast) -> OptMinimizeZ3<'ctx, 'opt> {
+        OptMinimizeZ3 {
+            ctx,
+            opt,
+            cst,
+            r: unsafe {
+                Z3_optimize_minimize(ctx.r, opt.r, cst);
             }
         }
     }
@@ -151,6 +207,114 @@ fn test_new_ocheck(){
 
     println!("Now we have an assert in the opt context, should print it.");
     println!("{}", GetOptStringZ3::new(&ctx, &opt).s);
+
+    let res1 = OptCheckZ3::new(&ctx, &opt, vec!());
+    println!("This is the return of the check:");
+    println!("{}", res1.r);
+
+    println!("This is the opt context with an assertion after the check:");
+    println!("{}", GetOptStringZ3::new(&ctx, &opt).s);
+
+    println!("Model: Should print the solution, we did a check.");
+    println!("{}", GetOptModelZ3::new(&ctx, &opt).s);
+}
+
+#[test]
+fn test_new_maximize(){
+    let conf = ConfigZ3::new();
+    let ctx = ContextZ3::new(&conf);
+    let opt = OptimizerZ3::new(&ctx);
+
+    let intsort = IntSortZ3::new(&ctx);
+    let x = IntVarZ3::new(&ctx, &intsort, "x");
+    let int1 = IntZ3::new(&ctx, &intsort, 100);
+
+    let lt1 = LTZ3::new(&ctx, x.r, int1.r);
+
+    OptAssertZ3::new(&ctx, &opt, lt1.r);
+    OptMaximizeZ3::new(&ctx, &opt, x.r);
+
+    println!("Now we have an assert in the opt context, should print it.");
+    println!("{}", GetOptStringZ3::new(&ctx, &opt).s);
+
+    println!("Model: Should print empty string, no check yet.");
+    println!("{}", GetOptModelZ3::new(&ctx, &opt).s);
+
+    let res1 = OptCheckZ3::new(&ctx, &opt, vec!());
+    println!("This is the return of the check:");
+    println!("{}", res1.r);
+
+    println!("This is the opt context with an assertion after the check:");
+    println!("{}", GetOptStringZ3::new(&ctx, &opt).s);
+
+    println!("Model: Should print the solution, we did a check.");
+    println!("{}", GetOptModelZ3::new(&ctx, &opt).s);
+}
+
+/// DESCRIPTION:
+///  - Decide about three activities (do or don't) and aim for maximum value
+///  - Need to choose at least activity 1 or 2 (or both)
+///  - The total time limit is 4 hours
+///     - Activity 1 takes 1 hour
+///     - Activity 2 takes 2 hours
+///     - Activity 3 takes 3 hours
+///  - Activity 3 is worth twice as much as activities 1 and 2
+///
+/// MODEL:
+///  - This can be modelled as a linear mixed-integer Problem
+///     - Binary variables x, y, z for activities 1, 2, 3
+///     - Linear constraint for time limit
+///     - Linear constraint for condition (1 or 2)
+///
+///     max x + y + 2z
+///     so that: x + 2y + 3z <= 4
+///              x + y >= 1
+///     where x, y, z in {0, 1}
+#[test]
+fn test_example_1(){
+    let conf = ConfigZ3::new();
+    let ctx = ContextZ3::new(&conf);
+    let opt = OptimizerZ3::new(&ctx);
+
+    let intsort = IntSortZ3::new(&ctx);
+    let x = IntVarZ3::new(&ctx, &intsort, "x");
+    let y = IntVarZ3::new(&ctx, &intsort, "y");
+    let z = IntVarZ3::new(&ctx, &intsort, "z");
+    let obj = IntVarZ3::new(&ctx, &intsort, "obj");
+
+    let zero = IntZ3::new(&ctx, &intsort, 0);
+    let one = IntZ3::new(&ctx, &intsort, 1);
+    let two = IntZ3::new(&ctx, &intsort, 2);
+    let three = IntZ3::new(&ctx, &intsort, 3);
+    let four = IntZ3::new(&ctx, &intsort, 4);
+
+    let twoy = MULZ3::new(&ctx, vec!(two.r, y.r));
+    let threez = MULZ3::new(&ctx, vec!(three.r, z.r));
+    let add1 = ADDZ3::new(&ctx, vec!(x.r, twoy.r, threez.r));
+
+    let constr1 = LEZ3::new(&ctx, add1.r, four.r);
+    let constr2 = GEZ3::new(&ctx, ADDZ3::new(&ctx, vec!(x.r, y.r)).r, one.r);
+    let add2 = ADDZ3::new(&ctx, vec!(x.r, y.r, MULZ3::new(&ctx, vec!(two.r, z.r)).r));
+    let constr3 = EQZ3::new(&ctx, obj.r, add2.r);    
+    
+    OptAssertZ3::new(&ctx, &opt, GEZ3::new(&ctx, x.r, zero.r).r);
+    OptAssertZ3::new(&ctx, &opt, GEZ3::new(&ctx, y.r, zero.r).r);
+    OptAssertZ3::new(&ctx, &opt, GEZ3::new(&ctx, z.r, zero.r).r);
+    OptAssertZ3::new(&ctx, &opt, LEZ3::new(&ctx, x.r, one.r).r);
+    OptAssertZ3::new(&ctx, &opt, LEZ3::new(&ctx, y.r, one.r).r);
+    OptAssertZ3::new(&ctx, &opt, LEZ3::new(&ctx, z.r, one.r).r);
+    
+    OptAssertZ3::new(&ctx, &opt, constr1.r);
+    OptAssertZ3::new(&ctx, &opt, constr2.r);
+    OptAssertZ3::new(&ctx, &opt, constr3.r);
+    
+    OptMaximizeZ3::new(&ctx, &opt, obj.r);
+
+    println!("Now we have an assert in the opt context, should print it.");
+    println!("{}", GetOptStringZ3::new(&ctx, &opt).s);
+
+    println!("Model: Should print empty string, no check yet.");
+    println!("{}", GetOptModelZ3::new(&ctx, &opt).s);
 
     let res1 = OptCheckZ3::new(&ctx, &opt, vec!());
     println!("This is the return of the check:");
