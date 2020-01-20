ced38641dd4fba1776c13e1e439f0c7051271505
Martin Dahl <martin.dahl@gmail.com>
2020-01-20 14:41:04 +0100

    temp

--- sp-runner/src/runners/helpers.rs
+++ sp-runner/src/runners/helpers.rs
@@ -295,119 +295,117 @@ fn sp_action_to_ex(a: &Action,
 //     assert_ne!(new_initial, Predicate::TRUE);
 // }
 
-// pub fn make_runner_model(model: &Model) -> RunnerModel {
+pub fn make_runner_model(model: &Model) -> RunnerModel {
 
-//     let (new_guards, new_initial) = extract_guards(&model, &Predicate::TRUE);
-
-
-//     let items = model.items();
-
-//     // find "ab" transitions from resources
-//     let resources: Vec<&Resource> = items
-//         .iter()
-//         .flat_map(|i| match i {
-//             SPItem::Resource(r) => Some(r),
-//             _ => None,
-//         })
-//         .collect();
-
-//     let trans: Vec<_> = resources.iter().flat_map(|r| r.make_global_transitions()).collect();
-
-//     let mut ab_ctrl: Vec<_> = trans.iter().filter(|t|t.controlled()).cloned().collect();
-//     ab_ctrl.iter_mut().for_each(|ct| {
-//         match new_guards.get(&ct.get_path().to_string()) {
-//             Some(t) => // AND with new guards.
-//             {
-//                 println!("UPDATING GUARD FOR TRANS {}", ct.get_path());
-//                 *ct.mut_guard() = Predicate::AND(vec![ct.guard().clone(), t.clone()]);
-//             },
-//             None => {},
-//         }
-//     });
-//     let ab_un_ctrl = trans.iter().filter(|t|!t.controlled()).cloned().collect();
-
-//     let preds: Vec<_> = resources.iter().flat_map(|r| r.make_global_state_predicates()).collect();
-
-//     // TODO: handle resource "sub items"
-
-//     // TODO: add global transitions.
+    // let (new_guards, new_initial) = extract_guards(&model, &Predicate::TRUE);
 
 
-//     // TODO: add global state.?
+    let items = model.items();
 
-//     // add global op transitions (all automatic for now).
-//     let global_ops: Vec<&Operation> = items
-//         .iter()
-//         .flat_map(|i| match i {
-//             SPItem::Operation(o) => Some(o),
-//             _ => None,
-//         })
-//         .collect();
+    // find "ab" transitions from resources
+    let resources: Vec<&Resource> = items
+        .iter()
+        .flat_map(|i| match i {
+            SPItem::Resource(r) => Some(r),
+            _ => None,
+        })
+        .collect();
 
-//     let global_ops_ctrl: Vec<_> = global_ops.iter().flat_map(|o|o.start()).cloned().collect();
-//     let global_ops_un_ctrl: Vec<_> = global_ops.iter().flat_map(|o|o.finish()).cloned().collect();
+    let trans: Vec<_> = resources.iter().flat_map(|r| r.make_global_transitions()).collect();
 
-//     let global_goals: Vec<IfThen> = global_ops.iter().flat_map(|o|o.goal().as_ref()).cloned().collect();
+    let mut ab_ctrl: Vec<_> = trans.iter().filter(|t|t.controlled()).cloned().collect();
+    // ab_ctrl.iter_mut().for_each(|ct| {
+    //     match new_guards.get(&ct.get_path().to_string()) {
+    //         Some(t) => // AND with new guards.
+    //         {
+    //             println!("UPDATING GUARD FOR TRANS {}", ct.get_path());
+    //             *ct.mut_guard() = Predicate::AND(vec![ct.guard().clone(), t.clone()]);
+    //         },
+    //         None => {},
+    //     }
+    // });
+    let ab_un_ctrl = trans.iter().filter(|t|!t.controlled()).cloned().collect();
 
-//     // println!("{:?}", global_ops);
+    let preds: Vec<_> = resources.iter().flat_map(|r| r.make_global_state_predicates()).collect();
 
-//     // println!("{:?}", resources);
+    // TODO: handle resource "sub items"
 
-//     let rm = RunnerModel {
-//         op_transitions: RunnerTransitions {
-//             ctrl: global_ops_ctrl,
-//             un_ctrl: global_ops_un_ctrl,
-//         },
-//         ab_transitions: RunnerTransitions {
-//             ctrl: ab_ctrl,
-//             un_ctrl: ab_un_ctrl,
-//         },
-//         plans: RunnerPlans::default(),
-//         state_predicates: preds,
-//         goals: global_goals,
-//         invariants: Vec::new(),
-//         model: model.clone(), // TODO: borrow?
-//     };
+    // TODO: add global transitions.
 
-//     crate::planning::generate_offline_nuxvm(&rm, &new_initial);
 
-//     return rm;
-// }
+    // TODO: add global state.?
 
-// pub fn make_initial_state(model: &Model) -> SPState {
-//     let items = model.items();
+    // add global op transitions (all automatic for now).
+    let global_ops: Vec<&Operation> = items
+        .iter()
+        .flat_map(|i| match i {
+            SPItem::Operation(o) => Some(o),
+            _ => None,
+        })
+        .collect();
 
-//     // find all variables in resources
-//     let resources: Vec<&Resource> = items
-//         .iter()
-//         .flat_map(|i| match i {
-//             SPItem::Resource(r) => Some(r),
-//             _ => None,
-//         })
-//         .collect();
+    let global_ops_ctrl: Vec<_> = global_ops.iter().flat_map(|o|o.start()).cloned().collect();
+    let global_ops_un_ctrl: Vec<_> = global_ops.iter().flat_map(|o|o.finish()).cloned().collect();
 
-//     let mut s = SPState::default();
+    let global_goals: Vec<IfThen> = global_ops.iter().flat_map(|o|o.goal().as_ref()).cloned().collect();
 
-//     for r in &resources {
-//         s.extend(r.make_initial_state());
-//     }
+    // println!("{:?}", global_ops);
 
-//     // add global state
-//     let vars: Vec<&Variable> = items
-//         .iter()
-//         .flat_map(|i| match i {
-//             SPItem::Variable(v) => Some(v),
-//             _ => None,
-//         })
-//         .collect();
+    // println!("{:?}", resources);
 
-//     for v in &vars {
-//         let _r = s.insert(&v.node().global_path().as_ref().unwrap().to_sp(),
-//                           AssignStateValue::SPValue(v.initial_value()));
-//     }
+    let rm = RunnerModel {
+        op_transitions: RunnerTransitions {
+            ctrl: global_ops_ctrl,
+            un_ctrl: global_ops_un_ctrl,
+        },
+        ab_transitions: RunnerTransitions {
+            ctrl: ab_ctrl,
+            un_ctrl: ab_un_ctrl,
+        },
+        plans: RunnerPlans::default(),
+        state_predicates: preds,
+        goals: global_goals,
+        invariants: Vec::new(),
+        model: model.clone(), // TODO: borrow?
+    };
 
-//     return s;
-// }
+    // crate::planning::generate_offline_nuxvm(&rm, &new_initial);
+
+    return rm;
+}
+
+pub fn make_initial_state(model: &Model) -> SPState {
+    let items = model.items();
+
+    // find all variables in resources
+    let resources: Vec<&Resource> = items
+        .iter()
+        .flat_map(|i| match i {
+            SPItem::Resource(r) => Some(r),
+            _ => None,
+        })
+        .collect();
+
+    let mut s = SPState::default();
+
+    for r in &resources {
+        s.extend(r.make_initial_state());
+    }
+
+    // add global state
+    let vars: Vec<&Variable> = items
+        .iter()
+        .flat_map(|i| match i {
+            SPItem::Variable(v) => Some(v),
+            _ => None,
+        })
+        .collect();
+
+    let state: Vec<_> = vars.iter().map(|v| (v.node().path().clone(), v.initial_value())).collect();
+    s.extend(SPState::new_from_values(&state[..]));
+
+    return s;
+}
 
 
 // modeling helpers
