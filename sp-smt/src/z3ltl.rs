83e703966b2b985fc9aaf4d49137cae593d48bbb
endre90 <endre90@gmail.com>
2020-03-06 00:17:57 +0100

    prototype temporal operator - until

--- sp-smt/src/z3ltl.rs
+++ sp-smt/src/z3ltl.rs
@@ -11,17 +11,17 @@ pub struct UntilZ3<'ctx> {
     pub y: Z3_ast
 }
 
-// test this somehow
+// test this somehow, current_unroll should be initialized with x in step 0
 impl <'ctx> UntilZ3<'ctx> {
-    pub fn new(ctx: &ContextZ3, ts_model: &TransitionSystemModel, x: &Predicate, y: &Predicate, mut from_step: u32, until_step: u32) -> Z3_ast {
-        let mut unrolled: Z3_ast = GetSPPredicateZ3::new(&ctx, ts_model, from_step, x);
-        if from_step <= until_step {
+    pub fn new(ctx: &ContextZ3, ts_model: &TransitionSystemModel, mut current_unroll: Z3_ast, 
+        x: &Predicate, y: &Predicate, mut from_step: u32, until_step: u32) -> Z3_ast {
+        let mut unrolled: Z3_ast = if from_step <= until_step {
             from_step = from_step + 1;
             unrolled = ORZ3::new(&ctx, vec!(
                     GetSPPredicateZ3::new(&ctx, ts_model, from_step - 1, y),
                     ANDZ3::new(&ctx, vec!(
                             GetSPPredicateZ3::new(&ctx, ts_model, from_step - 1, x),
-                            UntilZ3::new(&ctx, ts_model, &x, &y, from_step, until_step)
+                            UntilZ3::new(&ctx, ts_model, unrolled, &x, &y, from_step, until_step)
                         )
                     )
                 )
