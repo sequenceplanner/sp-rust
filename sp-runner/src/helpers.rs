b4075b79fd33f77871ed2271d6f023bb94f30e66
Martin Dahl <martin.dahl@gmail.com>
2020-08-11 11:40:58 +0200

    spit out one model checking problem per operation

--- sp-runner/src/helpers.rs
+++ sp-runner/src/helpers.rs
@@ -1,7 +1,6 @@
 use crate::formal_model::*;
 use sp_domain::*;
 use sp_runner_api::*;
-use std::collections::HashSet;
 
 // remember to also change in planner
 // TODO: make this constant crate-wide and use in more places
@@ -96,67 +95,51 @@ pub fn make_runner_model(model: &Model) -> RunnerModel {
 
 
     // debug low level model
+    let all_op_names = global_ops.iter().map(|o|o.name().to_string()).collect::<Vec<_>>();
     global_ops.iter().for_each(|o| {
+        // here we should find all unctronllable actions that can
+        // modify the variables of GUARD and disallow them.
+        println!("CHECKING OP: {}", o.name());
+        let mut temp_ts_model = ts_model.clone();
+
+        // remove "replan" spec
+        temp_ts_model.specs.retain(|s| s.path().parent().leaf() != "replan_specs");
+
+        let mut new_invariants = vec![];
+        temp_ts_model.transitions.retain(|t| {
+            //if t.actions.len() > 0 && t.actions.iter().any(|a| no_change.contains(&a.var)) {
+            if t.name() != o.name() && all_op_names.contains(&t.name().to_string()) {
+                if let Some(op) = global_ops.iter().find(|oo|t.name()==oo.name()) {
+                    println!("FOR OP: {}, filtering transition: {}", o.name(), t.path());
+                    if !t.controlled() && op.fvg != Predicate::TRUE {
+                        // this also means we need to forbid this state!
+                        new_invariants.push(op.fvg.clone());
+                    }
+                }
+                false
+            } else { true }
+        });
+
+        let new_specs = new_invariants.iter().map(|p| {
+            let i = Predicate::NOT(Box::new(p.clone()));
+            println!("REFINING: {}", i);
+            let ri = refine_invariant(&temp_ts_model, &i);
+            Spec::new("extended", ri)
+        }).collect::<Vec<_>>();
+        temp_ts_model.specs.extend(new_specs);
+
+        // here we check if the operation has an assertion
+        // which is assumed to be fulfilled for nominal behavior
+        // (eg "resource not in failure mode")
         let guard = if let Some(c) = o.fvc.as_ref() {
             Predicate::AND(vec![o.guard.clone(), c.clone()])
         } else {
             o.guard.clone()
         };
-        // here we should find all unctronllable actions that can
-        // modify the variables of GUARD and disallow them. TODO: we
-        // need to to backwards reachability to catch chains of
-        // transitions. this is just a test
-        let support = guard.support();
-        println!("CHECKING OP: {}, SUPPORT: {:?}", o.name(), support);
-        assert!(ts_model.transitions.len() > 0);
-        let forbidden = ts_model.transitions.iter().filter_map(|t| {
-            if t.name () == o.name() {
-                println!("SKIPPING OURSELF");
-                return None;
-            }
-            if t.actions.iter().any(|a| {
-                !t.controlled() && support.contains(&a.var)
-            }) {
-                Some(t.guard().clone())
-            } else {
-                None
-            }
-        }).collect::<Vec<_>>();
-
-        let extra_spec = Spec::new("other_unc",
-                                   Predicate::NOT(Box::new(Predicate::OR(forbidden))));
-
-        let mut temp_ts_model = ts_model.clone();
-        temp_ts_model.specs.push(extra_spec);
-
-        // remove replan spec
-        temp_ts_model.specs.retain(|s| s.path().parent().leaf() != "replan_specs");
-
-
-        // filter out any transitions that change unrelated variables, we dont need to check them
-        let modifies: HashSet<SPPath> = o.effects.iter().map(|a|a.var.clone()).collect();
-        let all: HashSet<SPPath> = ts_model_op.vars.iter().map(|v|v.path().clone()).collect();
-        let no_change: HashSet<&SPPath> = all.difference(&modifies).collect();
-
-        temp_ts_model.transitions.retain(|t| {
-            if
-            t.actions.iter().any(|a| all.contains(&a.var)) ||
-                t.effects.iter().any(|a| all.contains(&a.var)) {
-                    println!("FOR OP: {}, filtering transition: {}", o.name(), t.path());
-                    false
-                } else { true }
-        });
-
         let op = vec![(o.path().to_string(), guard, o.fvg.clone())];
         temp_ts_model.name += &format!("_{}", o.name());
         crate::planning::generate_offline_nuxvm_ctl(&temp_ts_model, &initial, &op);
     });
-
-    // let old_specs = ts_model.specs.clone();
-    // // let replan_specs: Vec<Spec> = ts_model.specs.iter().filter(|s| s.path().parent().leaf() == "replan_specs").cloned().collect();
-    // ts_model.specs.retain(|s| s.path().parent().leaf() != "replan_specs");
-    // crate::planning::generate_offline_nuxvm_ctl(&ts_model, &initial, &ops);
-    // ts_model.specs = old_specs;
 
     let mut un_ctrl: Vec<Transition> = ts_model
         .transitions
